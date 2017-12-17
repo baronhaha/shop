d653518bbb3a96e7522289711173a4754a8764f6
bullat2015 <bullat2015@mail.ru>
2017-12-18 01:15:18 +0300

    addsomethinggood

--- proba/settings.py
+++ proba/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'pv4yw9)iqp0iq6!)n!=)36!dv@v%q6wty4x(hzw-y%5bf&lw)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = []
 
 
@@ -144,6 +144,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_prod")
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static", "media")
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 
 try:
