d914c59b3f0ed42dce3f36849cc4cb9340d5276d
bullat2015 <bullat2015@mail.ru>
2017-12-18 01:05:12 +0300

    addsomethinggood

--- proba/urls.py
+++ proba/urls.py
@@ -17,13 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns += staticfiles_urlpatterns()
 
-if settings.DEBUG:
-    urlpatterns += patterns('django.contrib.staticfiles.views',
-        url(r'^static/(?P<path>.*)$', 'serve'),
-    )
 
 
 urlpatterns = [
