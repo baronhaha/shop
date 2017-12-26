a1bc5dcb8da34a7dbe19e8782f908381a6b73ae6
bullat2015 <bullat2015@mail.ru>
2017-12-26 05:57:04 +0300

    add_dump

--- proba/settings_prod.py
+++ proba/settings_prod.py
@@ -5,7 +5,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'db1',
-        'USER': 'golden',
+        'USER': 'gold',
         'PASSWORD': 'qop000qop',
         'HOST': 'localhost',
         'PORT': '',
