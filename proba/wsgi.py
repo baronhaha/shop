e7dcb22bf04f0292c87c1758569033721d8721a3
bullat2015 <bullat2015@mail.ru>
2017-12-26 06:07:30 +0300

    add_dump

--- proba/wsgi.py
+++ proba/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+import django.core.handlers.wsgi
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proba.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+application = django.core.handlers.wsgi.WSGIHandler()
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+
+
