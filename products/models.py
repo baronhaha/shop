f734995a6e1aa5b45649630b4a043a2fbf0e1a91
bullat2015 <bullat2015@mail.ru>
2018-01-04 15:30:26 +0300

    upgrade model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
 
 class ProductCategory(models.Model):
@@ -20,7 +21,7 @@ class Product(models.Model):
     discount = models.IntegerField(default=0)
     category = models.ForeignKey(ProductCategory, blank=True, null=True, default=None)
     short_description = models.TextField(blank=True, null=True, default=None)
-    description = models.TextField(blank=True, null=True, default=None)
+    description = RichTextUploadingField(blank=True, null=True, default=None)
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
