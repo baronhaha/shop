60d7880ab988950bbc7707a6ab5e0730acd7fefb
bullat2015 <bullat2015@mail.ru>
2018-03-09 16:00:20 +0300

    add short_content table

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=80) # заголовок поста
     datetime = models.DateTimeField(u'Дата публикации') # дата публикации
     content = RichTextUploadingField() # текст поста
+    short_content = RichTextUploadingField()
 
     def __unicode__(self):
         return self.title
