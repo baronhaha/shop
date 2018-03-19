3ab694175e764cefeae6f8ac87771cb0736fdea3
bullat2015 <bullat2015@mail.ru>
2018-03-20 00:04:52 +0300

    add particles.js

---
+++ static/static_dev/js/main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+
+    //
+    particlesJS.load('particles-js', '/static/static_dev/assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+});
