04900c549f340c049e3989bc937a8e0b6d7c9611
bullat2015 <bullat2015@mail.ru>
2018-04-01 15:44:24 +0300

    change pagination to 15

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 class PostsListView(ListView): # представление в виде списка
     model = Post               # модель для представления
-    paginate_by = 10
+    paginate_by = 15
 
 
 
